161e70f1e5c82d4de3cc5fd17cdb7113ba5dd6ce
Quentin Gérôme <geromequentin@gmail.com>
2024-02-07 12:10:17 +0100

    feat(S3): Make the base image compatible with a local hosting solution

--- images/base/files/scripts/fuse_mount.py
+++ images/base/files/scripts/fuse_mount.py
@@ -1,4 +1,6 @@
+import base64
 import http.server
+import json
 import multiprocessing
 import os
 import subprocess
@@ -10,24 +12,33 @@ import time
 # 2) using a token (that can be a short-lived one) served over HTTP
 # So we span an HTTP server to use the second option.
 # Yes, it's strange, but unless we want to fork gcsfuse, we will have to live with this
-GCS_TOKEN = os.environ.get("GCS_TOKEN", "")
+STORAGE_ENGINE_TYPE = os.environ.get("WORKSPACE_STORAGE_ENGINE", "gcp")
 WORKSPACE_BUCKET_NAME = os.environ.get("WORKSPACE_BUCKET_NAME", "")
-if GCS_TOKEN and WORKSPACE_BUCKET_NAME:
+
+if not WORKSPACE_BUCKET_NAME:
+    print("No WORKSPACE_BUCKET_NAME environment variable set, skipping Fuse mount")
+    exit(0)
+
+path_to_mount = "/home/jovyan/workspace"
+subprocess.run(["mkdir", "-p", path_to_mount], check=True)
+
+if STORAGE_ENGINE_TYPE == "gcp":
+    access_token = os.environ.get("WORKSPACE_STORAGE_ENGINE_GCP_ACCESS_TOKEN", "")
 
     class serveGCStoken(http.server.BaseHTTPRequestHandler):
         def do_GET(self):
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(bytes('{ "access_token": "' + GCS_TOKEN + '" }', "utf-8"))
+            self.wfile.write(
+                bytes('{ "access_token": "' + access_token + '" }', "utf-8")
+            )
 
     webServer = http.server.HTTPServer(("127.0.0.1", 4321), serveGCStoken)
     proc = multiprocessing.Process(target=webServer.serve_forever, args=())
     proc.start()
     time.sleep(0.5)
 
-    path_to_mount = "/home/jovyan/workspace"
-    subprocess.run(["mkdir", "-p", path_to_mount], check=True)
     args = [
         "gcsfuse",
         "-o",
@@ -47,3 +58,43 @@ if GCS_TOKEN and WORKSPACE_BUCKET_NAME:
         proc.terminate()
         time.sleep(0.5)
         proc.close()
+
+elif STORAGE_ENGINE_TYPE == "s3":
+    # b64("{}") == b'e30='
+    fuse_config = json.loads(
+        base64.b64decode(
+            os.environ.get("WORKSPACE_STORAGE_ENGINE_S3_FUSE_CONFIG", b"e30=")
+        )
+    )
+
+    aws_fuse_config = json.loads(
+        base64.b64decode(os.environ.get("AWS_S3_FUSE_CONFIG", b"e30="))
+    )
+    # tldr: dont use putenv https://docs.python.org/2/library/os.html#os.environ
+    os.environ["AWSACCESSKEYID"] = aws_fuse_config.get("AWS_ACCESS_KEY_ID", "")
+    os.environ["AWSSECRETACCESSKEY"] = aws_fuse_config.get("AWS_SECRET_ACCESS_KEY", "")
+    os.environ["AWSSESSIONTOKEN"] = aws_fuse_config.get("AWS_SESSION_TOKEN", "")
+
+    aws_endpoint = aws_fuse_config.get("AWS_ENDPOINT", "")
+    s3_is_minio = True if aws_endpoint else False
+
+    results = subprocess.run(
+        [
+            "s3fs",
+            WORKSPACE_BUCKET_NAME,
+            path_to_mount,
+            "-o",
+            "allow_other",
+            "-o",
+            "url=" + aws_endpoint,
+            # Debug
+            # "-o",
+            # "dbglevel=info",
+            # "-f",
+            # "-o",
+            # "curldbg",
+        ]
+        # always rw  # + (["-o", "ro"] if bucket["mode"] == "RO" else [])
+        # MinIO doesn't support the subdomain request style, use the older path request style.
+        + (["-o", "use_path_request_style"] if s3_is_minio else [])
+    )
