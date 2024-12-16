39cf14d231937d9d1e303d3642a71923dc206588
Quentin Gérôme <quentin@letsbuildit.dev>
2024-12-16 16:00:13 +0100

    fix(pip): Do not install deps in user space

--- images/base/files/scripts/bootstrap_pipeline.py
+++ images/base/files/scripts/bootstrap_pipeline.py
@@ -34,7 +34,7 @@ def run_pipeline(config):
 
         if os.path.exists("./pipeline/requirements.txt"):
             print("Installing requirements...")
-            os.system("pip install -r ./pipeline/requirements.txt --user")
+            os.system("pip install -r ./pipeline/requirements.txt")
 
         installed, uninstalled = version_info()
         if len(installed) > 0:
